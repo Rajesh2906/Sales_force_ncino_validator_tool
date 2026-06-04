14b91ded4397fdf66969a6052564c32d0844d4d1
Rajesh2906 <chagantipati.rajesh09@gmail.com>
2026-06-04 08:23:46 +0530

    base path updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/salesforce-validator/',
+  base: '/Sales_force_ncino_validator_tool/',
 })
